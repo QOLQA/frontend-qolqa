2e5309b242163806a6bb3dcc6125c93629f89153
Anghelo Alagon <200518@unsaac.edu.pe>
2026-05-27 00:50:55 -0500

    feat(entities/table): unlock FK dropdown with Toggle array option and label display

--- src/entities/table/ui/attribute-node.tsx
+++ src/entities/table/ui/attribute-node.tsx
@@ -11,16 +11,24 @@ import {
 } from "@fsd/shared/ui/dropdown-menu";
 import { MoreButton } from "@fsd/shared/ui/MoreButton";
 import { Delete, Edit } from "@fsd/shared/ui/icons/TableOptionsIcons";
+import { getColumnTypeLabel } from "@fsd/entities/table/lib";
 
 interface AttributeNodeProps {
 	column: Column;
 	columnId: string;
 	handleEdit: (column: Column) => void;
 	handleDelete: (columnId: string) => void;
+	handleToggleFkArray?: (columnId: string) => void;
 }
 
 export const AttributeNode = React.memo(
-	({ column, columnId, handleEdit, handleDelete }: AttributeNodeProps) => {
+	({
+		column,
+		columnId,
+		handleEdit,
+		handleDelete,
+		handleToggleFkArray,
+	}: AttributeNodeProps) => {
 		const handleEditClick = useCallback(() => {
 			handleEdit(column);
 		}, [handleEdit, column]);
@@ -33,15 +41,19 @@ export const AttributeNode = React.memo(
 			e.stopPropagation();
 		}, []);
 
+		const isFkColumn =
+			column.type === "FOREIGN_KEY" || column.type === "FOREIGN_KEY_ARRAY";
+
 		return (
 			<div className="table-attribute">
 				<span className="text-white">{column.name}</span>
 				<div>
-					<span className="text-lighter-gray">{column.type}</span>
+					<span className="text-lighter-gray">
+						{getColumnTypeLabel(column.type)}
+					</span>
 					<div className="table-attribute__options">
 						<ManagedDropdownMenu>
-							{column.type !== "PRIMARY_KEY" &&
-							column.type !== "FOREIGN_KEY" ? (
+							{column.type !== "PRIMARY_KEY" ? (
 								<DropdownMenuTrigger asChild>
 									<MoreButton
 										className="text-lighter-gray "
@@ -56,19 +68,33 @@ export const AttributeNode = React.memo(
 								side="right"
 								variant="menu-1"
 							>
-								<DropdownMenuItem type="normal" onClick={handleEditClick}>
-									<Edit className="text-white" />
-									Edit
-								</DropdownMenuItem>
-								<DropdownMenuSeparator className="bg-gray" />
-								<DropdownMenuItem
-									type="delete"
-									className="text-red"
-									onClick={handleDeleteClick}
-								>
-									<Delete className="text-red" />
-									Delete
-								</DropdownMenuItem>
+								{isFkColumn ? (
+									<DropdownMenuItem
+										type="normal"
+										onClick={() => handleToggleFkArray?.(columnId)}
+									>
+										Toggle array
+									</DropdownMenuItem>
+								) : (
+									<>
+										<DropdownMenuItem
+											type="normal"
+											onClick={handleEditClick}
+										>
+											<Edit className="text-white" />
+											Edit
+										</DropdownMenuItem>
+										<DropdownMenuSeparator className="bg-gray" />
+										<DropdownMenuItem
+											type="delete"
+											className="text-red"
+											onClick={handleDeleteClick}
+										>
+											<Delete className="text-red" />
+											Delete
+										</DropdownMenuItem>
+									</>
+								)}
 							</DropdownMenuContent>
 						</ManagedDropdownMenu>
 					</div>
