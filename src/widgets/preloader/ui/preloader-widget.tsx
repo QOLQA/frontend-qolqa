ba93755011904236eb0b85de6fb8406828d7040f
Anghelo Alagon <200518@unsaac.edu.pe>
2026-08-10 19:10:51 -0500

    fix(landing): adjust preloader content offset

--- src/widgets/preloader/ui/preloader-widget.tsx
+++ src/widgets/preloader/ui/preloader-widget.tsx
@@ -21,7 +21,7 @@ export const PreloaderWidget = () => {
           className="absolute -inset-13 aspect-square rounded-full border border-dashed border-[#0052cc]/60 shadow-[0_0_40px_rgba(0,82,204,0.4)] pointer-events-none"
         />
 
-        <div className="relative top-1/2 -translate-y-[10px] flex flex-col items-center gap-3">
+        <div className="relative top-1/2 -translate-y-[30px] flex flex-col items-center gap-3">
           {/* Logo de Qolqa */}
           <LogoSvg className="w-48 h-auto drop-shadow-[0_0_20px_rgba(0,82,204,0.8)] text-blue" />
 
